d9e0cdc8e2a4c0c5e71d30a5fa63a69744e0ab9a
wellitsabhi <abhishekongit@gmail.com>
2025-07-20 14:44:18 +0530

    05: Ai-Jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,21 @@
+import { openai, gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // donwload
-    await step.sleep("wait-a-moment", "30s");
-    // logs
-    await step.sleep("wait-a-moment", "10s");
-    // finalize
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAGent = createAgent({
+      name: "code-agent",
+      system: "You are an expert Next.js developer. You write readable, maintable code. You write simple Next.js and Reaxt.js code snippets.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+    const { output } = await codeAGent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+    // console.log(output);
+
+    return { output };
+  }
 );
